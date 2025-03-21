387ee6b624b947d76dbf7a870d8ebc6070af70fa
yash-parekh715 <parekhyash712005@gmail.com>
2025-03-21 16:28:29 +0530

    minor changes

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: `http://localhost:8080/api/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
