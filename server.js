80236642d2b97a6dc96270d48d21a5076addea42
yash-parekh715 <parekhyash712005@gmail.com>
2025-04-13 23:43:29 +0530

    minor changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:8080"],
     // origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
